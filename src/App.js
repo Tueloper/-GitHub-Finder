8bb049c1ca5a41d55e17ce3d2220578a6da676cd
Tueloper <ozurumbatochukwu@yahoo.com>
2020-04-14 17:33:11 +0100

    🎪 routes implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Notification from './components/notification/Notification';
-import './assets/css/App.css';
+import About from './components/pages/About';
+import './App.css';
 
 class App extends Component {
 	state = {
@@ -42,23 +44,40 @@ class App extends Component {
 	render () {
 		const { users, loading } = this.state;
 		return (
-			<div className='App'>
-				<Navbar />
-				<div className='container'>
-					<Notification alert={this.state.alert} />
-					<Search
-						searchUsers={this.searchUsers}
-						clearUsers={this.clearUsers}
-						showClear={
+			<Router>
+				<div className='App'>
+					<Navbar />
+					<div className='container'>
+						<Notification alert={this.state.alert} />
+						<Switch>
+							<Route
+								exact
+								path='/'
+								render={(props) => {
+									return (
+										<Fragment>
+											<Search
+												searchUsers={this.searchUsers}
+												clearUsers={this.clearUsers}
+												showClear={
 
-								users.length > 0 ? true :
-								false
-						}
-						setAlert={this.setAlert}
-					/>
-					<Users loading={loading} users={users} />
+														users.length > 0 ? true :
+														false
+												}
+												setAlert={this.setAlert}
+											/>
+											<Users loading={loading} users={users} />
+										</Fragment>
+									);
+								}}
+							/>
+
+							{/* About Page */}
+							<Route exact path='/about' component={About} />
+						</Switch>
+					</div>
 				</div>
-			</div>
+			</Router>
 		);
 	}
 }
